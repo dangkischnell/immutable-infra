8279c34a5a57816e9996334bc2e2c88dbeeffcc5
Your Name <you@example.com>
2023-03-27 01:36:43 +0200

    s

--- vm-based/infra/packer/output.tf
+++ vm-based/infra/packer/output.tf
@@ -1,6 +1,6 @@
-# output "SG_cidr" {
-#   value = "${aws_security_group.allow-ssh.cidr_blocks}"
-# }
+output "SG_cidr" {
+  value = "${aws_security_group.allow-ssh.cidr_blocks}"
+}
 
 output "subnet_cidr" {
   value = "${aws_subnet.packer.cidr_block }"
