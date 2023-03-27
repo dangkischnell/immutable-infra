1dd8ba26b5a4b16b18d4f454052714499cbc1b6c
Your Name <you@example.com>
2023-03-27 19:08:31 +0200

    s

--- vm-based/infra/packer/scripts/doing-something.sh
+++ vm-based/infra/packer/scripts/doing-something.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 echo "Put your works in here"
-=======
-#!/usr/bin/env bash
-echo "Put your works in here"
->>>>>>> upstream/main
--- vm-based/infra/packer/scripts/wait-cloud-init-complete.sh
+++ vm-based/infra/packer/scripts/wait-cloud-init-complete.sh
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 while ! grep "Cloud-init .* finished" /var/log/cloud-init.log; do
     echo "$(date -Ins) Waiting for cloud-init to finish"
     sleep 2
 done
-=======
-#!/usr/bin/env bash
-while ! grep "Cloud-init .* finished" /var/log/cloud-init.log; do
-    echo "$(date -Ins) Waiting for cloud-init to finish"
-    sleep 2
-done
->>>>>>> upstream/main
