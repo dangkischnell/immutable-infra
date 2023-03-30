06e867446ef2b2ea150502a96a50796d32862638
Your Name <you@example.com>
2023-03-30 23:38:34 +0200

    a

--- vm-based/infra/aws/scripts/init-deployment.bash
+++ vm-based/infra/aws/scripts/init-deployment.bash
@@ -5,5 +5,5 @@ date
 ansible-playbook \
   --inventory-file ansible-hosts.yml \
   --tags "deployments" \
-    "../../ansible/initial-deployment.yml" \
+    "../ansible/initial-deployment.yml" \
     ;
