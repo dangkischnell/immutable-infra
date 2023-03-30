5fd1f860630a4c5a3e7eb9e07a8a0f636622099b
Your Name <you@example.com>
2023-03-30 23:18:52 +0200

    s:

--- vm-based/infra/aws/provider.tf
+++ vm-based/infra/aws/provider.tf
@@ -1,5 +1,10 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = var.region
-  profile = var.profile
+variable "access_key" {}
+variable "secret_key" {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
